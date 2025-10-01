4c6123c24439b6ee639966d314b612766768d8ff
rexbur221 <jimkrauz@gmail.com>
2025-10-01 19:09:23 +0300

    added entity configurations

---
+++ src/DirectoryServiceSolution/Infrastructure/DirectoryService.Infrastructure.PostgreSQL/EntityFramework/ModelConfigurations/Departments/DepartmentConfiguration.cs
@@ -0,0 +1,62 @@
+using DirectoryService.Core.DeparmentsContext;
+using DirectoryService.Core.DeparmentsContext.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DirectoryService.Infrastructure.PostgreSQL.EntityFramework.ModelConfigurations.Departments;
+
+public sealed class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+{
+    public void Configure(EntityTypeBuilder<Department> builder)
+    {
+        builder.ToTable("departments");
+
+        builder.HasKey(d => d.Id);
+        builder.Property(d => d.Id)
+            .HasColumnName("id")
+            .HasConversion(toDb => toDb.Value, fromDb => DepartmentId.Create(fromDb));
+
+        builder.Property(d => d.Identifier)
+            .HasColumnName("identifier")
+            .HasConversion(toDb => toDb.Value, fromDb => DepartmentIdentifier.CreateNode(fromDb))
+            .HasMaxLength(DepartmentIdentifier.MaxLength);
+
+        builder.ComplexProperty(p => p.LifeCycle, cpb =>
+        {
+            cpb.Property(p => p.CreatedAt).HasColumnName("created_at");
+            cpb.Property(p => p.DeletedAt).HasColumnName("deleted_at");
+            cpb.Property(p => p.UpdatedAt).HasColumnName("updated_at");
+            cpb.Property(p => p.IsDeleted).HasColumnName("is_deleted");
+        });
+
+        builder.Property(d => d.Name)
+            .IsRequired()
+            .HasColumnName("name")
+            .HasMaxLength(DepartmentName.MaxLength)
+            .HasConversion(toDb => toDb.Value, fromDb => DepartmentName.Create(fromDb));
+
+        builder.Property(d => d.Path)
+            .IsRequired()
+            .HasColumnName("path")
+            .HasConversion(toDb => toDb.Value, fromDb => DepartmentPath.Create(fromDb));
+
+        builder.Property(d => d.Depth)
+            .IsRequired()
+            .HasConversion(toDb => toDb.Value, fromDb => DepartmentDepth.Create(fromDb));
+
+        builder.Property(d => d.Parent)
+            .HasConversion(toDb => toDb!.Value.Value, fromDb => DepartmentId.Create(fromDb))
+            .IsRequired(false);
+
+        builder.HasMany(l => l.Locations)
+            .WithOne(l => l.Department)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(l => l.Positions)
+            .WithOne(p => p.Department)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);                    
+    }
+
+}
---
+++ src/DirectoryServiceSolution/Infrastructure/DirectoryService.Infrastructure.PostgreSQL/EntityFramework/ModelConfigurations/Locations/LocationConfiguration.cs
@@ -0,0 +1,50 @@
+using DirectoryService.Core.LocationsContext;
+using DirectoryService.Core.LocationsContext.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DirectoryService.Infrastructure.PostgreSQL.EntityFramework.ModelConfigurations.Locations;
+
+public sealed class LocationConfiguration : IEntityTypeConfiguration<Location>
+{
+    public void Configure(EntityTypeBuilder<Location> builder)
+    {
+        builder.ToTable("locations");
+
+        builder.HasKey(l => l.Id);
+        builder.Property(l => l.Id)
+            .HasColumnName("id")
+            .HasConversion(toDb => toDb.Value, fromDb => new LocationId(fromDb));
+
+        builder.ComplexProperty(p => p.LifeCycle, cpb =>
+        {
+            cpb.Property(p => p.CreatedAt).HasColumnName("created_at");
+            cpb.Property(p => p.DeletedAt).HasColumnName("deleted_at");
+            cpb.Property(p => p.UpdatedAt).HasColumnName("updated_at");
+            cpb.Property(p => p.IsDeleted).HasColumnName("is_deleted");
+        });
+
+        builder.OwnsOne(l => l.Address, onb =>
+        {
+            onb.ToJson("address_parts");
+            onb.OwnsMany(l => l.Parts);
+        });
+
+        builder.Property(l => l.Name)
+            .HasColumnName("name")
+            .HasMaxLength(LocationName.MaxLength)
+            .IsRequired()
+            .HasConversion(toDb => toDb.Value, fromDb => LocationName.Create(fromDb));
+
+        builder.Property(l => l.TimeZone)
+            .HasColumnName("time_zone")
+            .IsRequired()
+            .HasConversion(toDb => toDb.Value, fromDb => LocationTimeZone.Create(fromDb));
+
+        builder.HasMany(l => l.Departments)
+            .WithOne(d => d.Location)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+
+}
