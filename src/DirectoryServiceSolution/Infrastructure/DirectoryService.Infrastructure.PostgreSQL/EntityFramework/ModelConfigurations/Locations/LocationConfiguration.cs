b5c6708608e16b624fdc668f2c63f848f93e2a0c
rexbur221 <jimkrauz@gmail.com>
2025-10-03 18:23:28 +0300

    fixed issue on exception when mapping jsonb Address property of Location entity

--- src/DirectoryServiceSolution/Infrastructure/DirectoryService.Infrastructure.PostgreSQL/EntityFramework/ModelConfigurations/Locations/LocationConfiguration.cs
+++ src/DirectoryServiceSolution/Infrastructure/DirectoryService.Infrastructure.PostgreSQL/EntityFramework/ModelConfigurations/Locations/LocationConfiguration.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using DirectoryService.Core.LocationsContext;
 using DirectoryService.Core.LocationsContext.ValueObjects;
 using Microsoft.EntityFrameworkCore;
@@ -29,14 +30,20 @@ public sealed class LocationConfiguration : IEntityTypeConfiguration<Location>
             }
         );
 
-        builder.OwnsOne(
-            l => l.Address,
-            onb =>
-            {
-                onb.ToJson("address_parts");
-                onb.OwnsMany(l => l.Parts);
-            }
-        );
+        builder
+            .Property(l => l.Address)
+            .HasColumnType("jsonb")
+            .HasConversion(ad => AddressToJsonb(ad), jsonb => AddressFromJsonb(jsonb))
+            .HasColumnName("address");
+
+        // builder.OwnsOne(
+        //     l => l.Address,
+        //     onb =>
+        //     {
+        //         onb.ToJson("address_parts");
+        //         onb.OwnsMany(l => l.Parts);
+        //     }
+        // );
 
         builder
             .Property(l => l.Name)
@@ -50,5 +57,23 @@ public sealed class LocationConfiguration : IEntityTypeConfiguration<Location>
             .HasColumnName("time_zone")
             .IsRequired()
             .HasConversion(toDb => toDb.Value, fromDb => LocationTimeZone.Create(fromDb));
+    }
+
+    private static string AddressToJsonb(LocationAddress address)
+    {
+        return JsonSerializer.Serialize(address.Parts.Select(p => p.Node));
+    }
+
+    private static LocationAddress AddressFromJsonb(string jsonb)
+    {
+        using JsonDocument document = JsonDocument.Parse(jsonb);
+        List<LocationAddressPart> parts = [];
+        foreach (JsonElement entry in document.RootElement.EnumerateArray())
+        {
+            string? addressPart = entry.GetString();
+            if (!string.IsNullOrWhiteSpace(addressPart))
+                parts.Add(LocationAddressPart.Create(addressPart));
+        }
+        return LocationAddress.Create(parts);
     }
 }
