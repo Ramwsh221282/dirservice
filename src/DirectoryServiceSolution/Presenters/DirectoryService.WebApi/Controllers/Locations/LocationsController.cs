6007c70d6a689b9fc32d0bcb274f7b39a3d23102
rexbur221 <jimkrauz@gmail.com>
2025-10-03 22:02:07 +0300

    adjusted locations controller class for contract DTO

--- src/DirectoryServiceSolution/Presenters/DirectoryService.WebApi/Controllers/Locations/LocationsController.cs
+++ src/DirectoryServiceSolution/Presenters/DirectoryService.WebApi/Controllers/Locations/LocationsController.cs
@@ -1,3 +1,4 @@
+using DirectoryService.Contracts;
 using DirectoryService.UseCases.Locations.CreateLocation;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,12 +10,11 @@ public sealed class LocationsController : ControllerBase
 {
     [HttpPost("")]
     public async Task<IResult> CreateLocation(
-        [FromBody] CreateLocationRequest request,
+        [FromBody] CreateLocationCommand command,
         [FromServices] CreateLocationCommandHandler handler,
         CancellationToken ct
     )
     {
-        CreateLocationCommand command = request.AsCommand();
         Guid result = await handler.Handle(command, ct);
         return Results.Ok(result);
     }
