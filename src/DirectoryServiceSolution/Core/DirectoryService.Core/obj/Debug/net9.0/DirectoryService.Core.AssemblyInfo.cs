5533ced5b51e216f4005b7268a0557ad53ee3a04
Ramwsh221282 <jimkrauz@gmail.com>
2025-10-04 13:46:12 +0700

    added department identifier VO adaptation for result pattern

--- src/DirectoryServiceSolution/Core/DirectoryService.Core/obj/Debug/net9.0/DirectoryService.Core.AssemblyInfo.cs
+++ src/DirectoryServiceSolution/Core/DirectoryService.Core/obj/Debug/net9.0/DirectoryService.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DirectoryService.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2440e072f3b50bb0a2ca1dcf8f249fbe63ce8613")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47bb886720f0c6427234adb43d03834470235178")]
 [assembly: System.Reflection.AssemblyProductAttribute("DirectoryService.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DirectoryService.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
