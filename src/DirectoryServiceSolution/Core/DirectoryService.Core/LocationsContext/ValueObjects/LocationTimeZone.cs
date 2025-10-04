d03f9af3b3dbc8029a6ddce877a7bb7b1aa324d0
rexbur221 <jimkrauz@gmail.com>
2025-10-04 20:59:54 +0700

    added location time zone class VO adaptation for result pattern plus private constructor

--- src/DirectoryServiceSolution/Core/DirectoryService.Core/LocationsContext/ValueObjects/LocationTimeZone.cs
+++ src/DirectoryServiceSolution/Core/DirectoryService.Core/LocationsContext/ValueObjects/LocationTimeZone.cs
@@ -1,4 +1,5 @@
 using DirectoryService.Core.Common.Extensions;
+using ResultLibrary;
 
 namespace DirectoryService.Core.LocationsContext.ValueObjects;
 
@@ -7,20 +8,20 @@ public sealed record LocationTimeZone
     public const int MaxLength = 100;
     public string Value { get; }
 
-    public LocationTimeZone(string value)
-    {
-        Value = value;
-    }
+    private LocationTimeZone(string value) => Value = value;
 
-    public static LocationTimeZone Create(string value)
+    public static Result<LocationTimeZone> Create(string value)
     {
         if (string.IsNullOrWhiteSpace(value))
-            throw new ArgumentException("Временная зона IANA была пустой.");
+            return Error.ValidationError("Временная зона IANA была пустой.");
+
         if (value.GreaterThan(MaxLength))
-            throw new ArgumentException("Временная зона IANA некорректна.");
+            return Error.ValidationError("Временная зона IANA некорректна.");
+
         string[] parts = value.Split('/');
         if (parts.Length != 2)
-            throw new ArgumentException("Временная зона IANA некорректна.");
+            return Error.ValidationError("Временная зона IANA некорректна.");
+
         parts[0] = parts[0].FormatForName();
         parts[1] = parts[1].FormatForName();
         return new LocationTimeZone(string.Join('/', parts));
