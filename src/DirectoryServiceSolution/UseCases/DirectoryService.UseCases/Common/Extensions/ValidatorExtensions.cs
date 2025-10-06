9549669bfb24079b0153d549646bc0311c73bc84
Ramwsh221282 <jimkrauz@gmail.com>
2025-10-06 13:37:27 +0700

    added create location command validator class

---
+++ src/DirectoryServiceSolution/UseCases/DirectoryService.UseCases/Common/Extensions/ValidatorExtensions.cs
@@ -0,0 +1,53 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using ResultLibrary;
+
+namespace DirectoryService.UseCases.Common.Extensions;
+
+public static class ValidatorExtensions
+{
+    public static IRuleBuilderOptionsConditions<T, TU> MustBeValid<T, TU>(
+        this IRuleBuilderInitial<T, TU> rule,
+        Func<TU, Result> resultFactory
+    )
+    {
+        return rule.Custom(
+            (property, context) =>
+            {
+                Result result = resultFactory(property);
+                if (result.IsFailure)
+                {
+                    ValidationFailure failure = new ValidationFailure()
+                    {
+                        ErrorMessage = result.Error.Message,
+                    };
+                    context.AddFailure(failure);
+                }
+            }
+        );
+    }
+
+    public static IRuleBuilderOptionsConditions<T, IEnumerable<TU>> AllMustBeValid<T, TU>(
+        this IRuleBuilderInitial<T, IEnumerable<TU>> rule,
+        Func<TU, Result> resultFactory
+    )
+    {
+        return rule.Custom(
+            (properties, context) =>
+            {
+                foreach (var property in properties)
+                {
+                    Result result = resultFactory(property);
+                    if (result.IsFailure)
+                    {
+                        ValidationFailure failure = new ValidationFailure()
+                        {
+                            ErrorMessage = result.Error.Message,
+                        };
+                        context.AddFailure(failure);
+                    }
+                }
+            }
+        );
+    }
+}
---
+++ src/DirectoryServiceSolution/UseCases/DirectoryService.UseCases/Locations/CreateLocation/CreateLocationCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using DirectoryService.Core.LocationsContext.ValueObjects;
+using DirectoryService.UseCases.Common.Extensions;
+using FluentValidation;
+
+namespace DirectoryService.UseCases.Locations.CreateLocation;
+
+public sealed class CreateLocationCommandValidator : AbstractValidator<CreateLocationCommand>
+{
+    public CreateLocationCommandValidator()
+    {
+        RuleFor(x => x.Name).MustBeValid(LocationName.Create);
+        RuleFor(x => x.TimeZone).MustBeValid(LocationTimeZone.Create);
+        RuleFor(x => x.AddressParts).AllMustBeValid(LocationAddressPart.Create);
+    }
+}
