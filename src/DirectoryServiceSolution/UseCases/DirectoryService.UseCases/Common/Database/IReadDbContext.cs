f87aa8cd007cf4281602e505006e1412b8a2ae97
rexbur221 <jimkrauz@gmail.com>
2025-10-12 20:31:03 +0700

    удален ReadDbContext

--- src/DirectoryServiceSolution/UseCases/DirectoryService.UseCases/Common/Database/IReadDbContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using DirectoryService.Core.LocationsContext;
-
-namespace DirectoryService.Infrastructure.PostgreSQL.EntityFramework;
-
-public interface IReadDbContext
-{
-    IQueryable<Location> LocationsRead { get; }
-}
