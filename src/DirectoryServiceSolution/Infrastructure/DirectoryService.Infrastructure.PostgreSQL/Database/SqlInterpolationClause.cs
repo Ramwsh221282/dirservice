79f51baf2fae8ae05c5cc456c1d134c3a27efb14
rexbur221 <jimkrauz@gmail.com>
2025-10-12 20:29:27 +0700

    рефакторинг обработчика. Добавление абстракции IQueryClause

---
+++ src/DirectoryServiceSolution/Infrastructure/DirectoryService.Infrastructure.PostgreSQL/Database/SqlInterpolationClause.cs
@@ -0,0 +1,56 @@
+﻿using System.Data;
+using Dapper;
+
+namespace DirectoryService.Infrastructure.PostgreSQL.Database;
+
+public sealed class SqlInterpolationClause : IQueryClause
+{
+    private readonly DynamicParameters _parameters = new();
+    private readonly List<string> _sqlAdditions = [];
+
+    public IQueryClause AddClause<T>(string sql, string parameterName, T value, DbType dbType)
+    {
+        _sqlAdditions.Add(sql);
+        _parameters.Add(parameterName, value, dbType);
+        return this;
+    }
+
+    public IQueryClause AddClause<T>(string sql, string parameterName, T value)
+    {
+        _sqlAdditions.Add(sql);
+        _parameters.Add(parameterName, value);
+        return this;
+    }
+
+    public IQueryClause AddClause(string sql)
+    {
+        _sqlAdditions.Add(sql);
+        return this;
+    }
+
+    public IQueryClause AddClause(string sql, string parameterName, object value, DbType dbType)
+    {
+        _sqlAdditions.Add(sql);
+        _parameters.Add(parameterName, value, dbType);
+        return this;
+    }
+
+    public IQueryClause AddClause(string sql, string parameterName, object value)
+    {
+        _sqlAdditions.Add(sql);
+        _parameters.Add(parameterName, value);
+        return this;
+    }
+
+    public string FormSqlClause(string prefix, string separator)
+    {
+        return _sqlAdditions.Count == 0
+            ? string.Empty
+            : prefix + string.Join(separator, _sqlAdditions);
+    }
+
+    public string FormRawClause(string sql)
+    {
+        return _sqlAdditions.Count == 0 ? string.Empty : sql;
+    }
+}
---
+++ src/DirectoryServiceSolution/UseCases/DirectoryService.UseCases/Common/Database/IQueryClause.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace DirectoryService.Infrastructure.PostgreSQL.Database;
+
+public interface IQueryClause
+{
+    IQueryClause AddClause<T>(string sql, string parameterName, T value, DbType dbType);
+    IQueryClause AddClause<T>(string sql, string parameterName, T value);
+    IQueryClause AddClause(string sql);
+    IQueryClause AddClause(string sql, string parameterName, object value, DbType dbType);
+    IQueryClause AddClause(string sql, string parameterName, object value);
+}
